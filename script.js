f0ad407b77d1ed2d52f2aaa008c97018cf7ecb7f
Muhammad Ammar Cheema <muhammadammar7747@gmail.com>
2023-10-29 20:37:13 +0500

    Header Responsiveness

--- script.js
+++ script.js
@@ -111,3 +111,17 @@ function populateSanitizationForm(){
         });
     }
 }
+
+const menuToggle = document.getElementById('menu-toggle');
+const navList = document.getElementById('nav-list');
+
+menuToggle.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
+
+const navLinks = document.querySelectorAll('.nav-list a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navList.classList.remove('active');
+    });
+});
