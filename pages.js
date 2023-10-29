5d1f21e1410867f90f6a1589e3404cc8137685eb
Siddharth Mittal <siddharth.mittal31@gmail.com>
2023-10-29 13:13:12 -0700

    zip code validation

--- pages.js
+++ pages.js
@@ -13,6 +13,11 @@ const validationForms = [
         "title": "Credit Card Validation",
         "explanation": "This regular expression should match common credit cards such as Visa, Mastercard, AMEX, etc."
         "regexPattern": "^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$"
+    },
+    {
+        "title": "US Zip Code Validation",
+        "explanation": "This regular expression should match zip codes in the United States"
+        "regexPattern": "/(^\d{5}$)|(^\d{5}-\d{4}$)/"
     }
 ];
 
