39fc681d3ac85bccf15f8e7fe4997d7f16107bd3
farehiqbal <fareh.iqbal55@gmail.com>
2023-10-31 15:03:39 +0500

    added credit card validation
    Added credit card regex that can detect the 6 major credit card patterns.

--- pages.js
+++ pages.js
@@ -4,6 +4,12 @@ const validationForms = [
         "explanation": "This regular expression should match a 4-digit number.",
         "regexPattern": "^\\d{4}$"
     },
+
+    {
+        "title": "Credit card number",
+        "explanation": "This regular expression should match a credit card number. It should match Visa, MasterCard, American Express, Diners Club, Discover, and JCB cards.",
+        "regexPattern": "(^4[0-9]{12}(?:[0-9]{3})?$)|(^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$)|(3[47][0-9]{13})|(^3(?:0[0-5]|[68][0-9])[0-9]{11}$)|(^6(?:011|5[0-9]{2})[0-9]{12}$)|(^(?:2131|1800|35\d{3})\d{11}$)"
+    },
 ];
 
 const extractionForms = [
